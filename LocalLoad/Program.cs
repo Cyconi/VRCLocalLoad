3b0c7a981b7aec3d72aa73c43888ebee375fa527
Athen <cyconiexo@gmail.com>
2024-07-16 10:45:05 -0700

    default file renames

--- LocalLoad/Program.cs
+++ LocalLoad/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 
 class Program
 {
@@ -20,11 +21,26 @@ class Program
 
         // Display the files and let the user select one
         for (int i = 0; i < files.Length; i++)
-            Console.WriteLine($"{i + 1}. {Path.GetFileName(files[i])}");
+        {
+            var fileName = Path.GetFileName(files[i]);
+
+            if (fileName.Contains("wrld_4432ea9b-729c-46e3-8eaf-846aa0a37fdd"))
+                fileName = fileName.Replace("wrld_4432ea9b-729c-46e3-8eaf-846aa0a37fdd", "VRChat Home");
+
+            if (fileName.Contains("wrld_ba913a96-fac4-4048-a062-9aa5db092812"))
+                fileName = fileName.Replace("wrld_ba913a96-fac4-4048-a062-9aa5db092812", "VRChat Hub");
+
+            if (fileName.Contains("wld_7d3d25ec-663e-406e-96a3-e2c4fc0d8104"))
+                fileName = fileName.Replace("wld_7d3d25ec-663e-406e-96a3-e2c4fc0d8104", "Tutorial");
+
+
+            Console.WriteLine($"{i + 1}. {fileName}");
+        }
+            
         
         Console.Write("Select a file by entering its number (leave blank = 1): ");
         string fileInput = Console.ReadLine();
-        int selectedIndex = string.IsNullOrEmpty(fileInput) ? 0 : int.Parse(fileInput) - 1;
+        int selectedIndex = string.IsNullOrEmpty(fileInput) || int.Parse(fileInput) == 0 ? 0 : int.Parse(fileInput) - 1;
         string path = files[selectedIndex];
 
         Console.Write("Amount of Instances to Create (leave blank = 1): ");
